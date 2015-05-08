da66763e36493b8c80afa45b7be35bfaf8bb1272
DrEmixam <maxime@melinon.com>
2015-05-08 20:18:19 +0200

    Changements pour pouvoir packager

--- lsclock.js
+++ lsclock.js
@@ -7,10 +7,6 @@ var
   server = require('http').createServer(app), // create webserver
   net = require('net');
 
-var config = {
-  unixSocket: __dirname + '/lsclock.sock'
-};
-
 app.use(express.static(__dirname + '/static'));
 
 app.get('/getDate', function (req, res) {
@@ -21,30 +17,10 @@ app.get('/getDate', function (req, res) {
 
 app.get('/', function (req, res) {
   if (req.acceptsLanguage('fr')) {
-    res.sendfile('static/home.fr.html');
+    res.sendfile(__dirname + '/static/home.fr.html');
   } else {
-    res.sendfile('static/home.html');
+    res.sendfile(__dirname + '/static/home.html');
   }
 });
 
-server.on('error', function (e) {
-  if (e.code === 'EADDRINUSE') {
-    var clientSocket = new net.Socket();
-    clientSocket.on('error', function (e) { // handle error trying to talk to server
-      if (e.code === 'ECONNREFUSED') { // No other server listening
-        fs.unlinkSync(config.unixSocket);
-        server.listen(config.unixSocket, function () { //'listening' listener
-          console.log('server recovered');
-        });
-      }
-    });
-    clientSocket.connect({
-      path: config.unixSocket
-    }, function () {
-      console.log('Server running, giving up...');
-      process.exit();
-    });
-  }
-});
-
-server.listen(config.unixSocket);
+server.listen(3000);
